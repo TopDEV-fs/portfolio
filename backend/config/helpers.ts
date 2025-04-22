62440044494996f89a5d6b5153ead7a7af982f82
topDEV-fs <tpdvlpr@gmail.com>
2025-04-22 14:13:51 -0400

    fixed backend helper

--- backend/config/helpers.ts
+++ backend/config/helpers.ts
@@ -13,7 +13,11 @@ const connectWithDB = (): void => {
   if (mongoose.connection.readyState >= 1) {
     return;
   }
-
+  mongoose.set('strictQuery', true);
+  if (!process.env.MONGO_URI) {
+    throw new Error('Mongo URI is not defined in environment variables');
+  }
+  console.log(process.env.MONGO_URI, 'mongo uri');
   mongoose.connect(process.env.MONGO_URI!, {}, (err) => {
     if (err) throw err;
     console.log('Connected to DB');
