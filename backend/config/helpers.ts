c7405e18fc8098581e1373fa1d939662a3b36787
topDEV-fs <tpdvlpr@gmail.com>
2025-04-22 14:32:15 -0400

    fixed helper.ts for db integration

--- backend/config/helpers.ts
+++ backend/config/helpers.ts
@@ -10,11 +10,11 @@ export { connectWithDB };
  *@instantiates connection with MongoDB
  */
 
+mongoose.set('strictQuery', true);
 const connectWithDB = (): void => {
   if (mongoose.connection.readyState >= 1) {
     return;
   }
-  mongoose.set('strictQuery', true);
   if (!process.env.MONGO_URI) {
     throw new Error('Mongo URI is not defined in environment variables');
   }
